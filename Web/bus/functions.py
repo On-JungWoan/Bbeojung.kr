5a6bec1681c1ee20294090e1c1ec97a6b4674c01
on-jungwoan <slalfpdl16@naver.com>
2022-10-27 00:54:05 +0900

    detail 페이지 css/js/html 수정

--- Web/bus/functions.py
+++ Web/bus/functions.py
@@ -1,6 +1,26 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+
+from Private.secret import *
 from lightgbm import LGBMRegressor
 import joblib
 import numpy as np
+
+import requests
+import json
+
+# # url 입력
+# BUS_ID = 2873
+# url = f'http://api.gwangju.go.kr/xml/arriveInfo?serviceKey={BIS_ARRIVE_KEY}&BUSSTOP_ID={BUS_ID}'
+
+# # url 불러오기
+# response = requests.get(url)
+
+# #데이터 값 출력해보기
+# contents = response.text
+# json_ob = json.loads(contents)
+
 
 MODEL_PATH = './Model/lgbm_4th.pkl'
 model = joblib.load(MODEL_PATH)
