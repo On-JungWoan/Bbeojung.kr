ed0dcfd895ba9210d19ad6091122a9da951afc63
on-jungwoan <slalfpdl16@naver.com>
2022-10-26 22:36:10 +0900

    캐시 삭제 및 재커밋

--- Web/Model/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import joblib
-
-MODEL_PATH = 'C:/Users/user/Desktop/bj/Bbeojung.kr/Web/Model'
-MODEL_NAME = 'lgbm_second.pkl'
-
-
-model = joblib.load(os.path.join(MODEL_PATH, MODEL_NAME))
-model
